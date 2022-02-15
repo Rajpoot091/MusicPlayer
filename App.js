425aaf2881525125988161f820207587a80272e2
Muhammad Asad <asadrajpoot0304@gmail.com>
2022-02-15 16:21:29 +0500

    Initial comit

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import MusicPlayer from "./model/MusicPlayer";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <MusicPlayer />
       <StatusBar style="auto" />
     </View>
@@ -16,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
 });
